fe9b1860d9ccef5076b8d46fb52949ae04565059
fcostas <flp.costas@gmail.com>
2014-05-01 01:17:14 -0300

    criando id manualmente

--- src/flpcostas/manual/ProgramManual.java
+++ src/flpcostas/manual/ProgramManual.java
@@ -5,20 +5,22 @@ import flpcostas.dominio.Livro;
 
 /**
  * Executando ID manualmente
- * 
+ * Injecao de dependencia mais basica possivel
  * Com base nas duas classes de dominio criada. Autor e Livro
  * 
  * @author Filipe
- *
+ * 
  */
 public class ProgramManual {
 
 	public static void main(String[] args) {
 
-		//precisar criar o autor
+		// precisar criar o autor
 		Autor autor = new Autor();
-		//fica claro que livro depende de um autor...
+
+		// fica claro que livro depende de um autor...
 		Livro livro = new Livro(autor);
+		System.out.println(livro.getAutor().getClass());
 	}
-	
+
 }
